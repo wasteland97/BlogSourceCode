bf2b9332682f8ff7caa65c45d6dd385c106c434a
Wilf <wfh970717@163.com>
2025-07-08 01:53:27 +0800

    feat(blog):"增加博客源代码"

---
+++ BlogSourceCode/src/main/java/com/wasteland/blogsourcecode/AiCodeReview/TestAi.java
@@ -0,0 +1,33 @@
+package com.wasteland.blogsourcecode.AiCodeReview;
+
+import com.wasteland.blogsourcecode.debugDemo.Student;
+import org.assertj.core.util.Lists;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * @author wfhstart
+ * @create 2025-07-08
+ */
+public class TestAi {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setAge(18);
+        student.setName("张三");
+        student.setAddress("浙江");
+        Student student1 = new Student();
+        student1.setAge(19);
+        student1.setName("李四");
+        student1.setAddress("李四");
+
+        List<Student> studentList = Lists.newArrayList(student, student1);
+
+        List<String> nameList = studentList.stream().filter(Objects::nonNull)
+                .filter(it -> it.getAge() > 18)
+                .map(Student::getName)
+                .collect(Collectors.toList());
+        System.out.println(nameList);
+    }
+}
