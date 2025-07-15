bc710dc18dca1ad1abfd38f36617d4e2575e0276
Wilf <wfh970717@163.com>
2025-07-15 22:33:49 +0800

    feat(blog):"测试java"

--- BlogSourceCode/src/main/java/com/wasteland/blogsourcecode/AiCodeReview/TestAi.java
+++ BlogSourceCode/src/main/java/com/wasteland/blogsourcecode/AiCodeReview/TestAi.java
@@ -73,4 +73,14 @@ public class TestAi {
         }
         return res;
     }
+
+    private static int count4(int number) {
+        int res = 0;
+        for (int i = 0; i < number; i++) {
+            if (i == 44) {
+                res += i;
+            }
+        }
+        return res;
+    }
 }
